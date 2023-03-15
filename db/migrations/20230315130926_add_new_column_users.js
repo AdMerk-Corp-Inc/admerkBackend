8f7d475759f54e94a28244983aa86d152eaa8658
idris-kabri <kabriidris53@gmail.com>
2023-03-15 20:33:29 +0530

    changes

--- db/migrations/20230315130926_add_new_column_users.js
+++ db/migrations/20230315130926_add_new_column_users.js
@@ -5,12 +5,15 @@
 exports.up = function(knex) {
     return knex.schema.table('users', table => {
         table.string("user_type")
-        table.string("state")
-        table.string("city")
+        table.string("state_id")
+        table.string("state_name")
+        table.string("city_id")
+        table.string("city_name")
         table.string("zip_code")
         table.string("18+")
         table.string("citizenship")
         table.string("sponsor_cartegory")
+        table.string("sponsor_group")
     })
 };
 
@@ -21,11 +24,14 @@ exports.up = function(knex) {
 exports.down = function(knex) {
     return knex.schema.table('users', table => {
         table.dropColumn('user_type');
-        table.dropColumn('state');
-        table.dropColumn('city');
+        table.string("state_id")
+        table.string("state_name")
+        table.string("city_id")
+        table.string("city_name")
         table.dropColumn('zip_code');
         table.dropColumn('18+');
         table.dropColumn('citizenship');
         table.dropColumn('sponsor_cartegory');
+        table.dropColumn('sponsor_group');
       })
 };
