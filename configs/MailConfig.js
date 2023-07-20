c2091784657f02752a2a5a6eac2a431c25514fdc
Muhammad Ahmad Tahir <muhammadahmadtahir9999@gmail.com>
2023-07-20 21:54:06 +0500

    fuck you

--- configs/MailConfig.js
+++
@@ -1,8 +0,0 @@
-exports.provider = "smtp"; // provider name : gmail, smtp, sendmail
-// mail configuration for gmail or smtp
-exports.hostName = "mail.webamplifierapps.com";
-exports.port = 587;
-exports.isSecure = false; // true for 465, false for other ports
-exports.username = "support@webamplifierapps.com";
-exports.password = "F%+103w3GS3I";
-exports.fromEmail = "support@admerkcorp.com";
